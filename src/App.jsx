fb31942ab6c9ac91d6a0d45ecf14377419ac298f
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-08-22 16:30:09 +0300

    Fix Fade in

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ console.log(visible);
       </main>
       <main className="second-page">
         <div className="about-us">
-          <div ref={ref} className={`hidden about-us-container one-section ${visible}`}>
+          <div  className={`hidden about-us-container one-section ${visible}`}>
             <h1 className="big-tittles">| Tittle |</h1>
             <p className="text-about">Whether you need high quality imagery, a compelling story or marketing strategy – we’ve got you covered. What about a mountain location with perfect conditions that cannot be imitated, regardless of its whereabouts? No problem. All this while hosting world-class athletes or talent? Of course.</p>
           </div>
@@ -73,7 +73,7 @@ console.log(visible);
           <h1 className="big-tittles">| Tittle |</h1>
             <p className="text-about">Whether you need high quality imagery, a compelling story or marketing strategy – we’ve got you covered. What about a mountain location with perfect conditions that cannot be imitated, regardless of its whereabouts? No problem. All this while hosting world-class athletes or talent? Of course.</p>
           </div>
-          <div ref={ref} className={`hidden about-us-container three-section ${visible}`}>  <h1 className="big-tittles">| Tittle |</h1>
+          <div  className={`hidden about-us-container three-section ${visible}`}>  <h1 className="big-tittles">| Tittle |</h1>
             <p className="text-about">Whether you need high quality imagery, a compelling story or marketing strategy – we’ve got you covered. What about a mountain location with perfect conditions that cannot be imitated, regardless of its whereabouts? No problem. All this while hosting world-class athletes or talent? Of course.</p></div>
         </div>
       </main>
