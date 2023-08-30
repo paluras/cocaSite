1080c8b12bfb7bc5e7902b519e688b98de7879e0
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-08-30 10:24:41 +0300

    Style add

--- components/footer/footer.component.jsx
+++ components/footer/footer.component.jsx
@@ -10,14 +10,14 @@ function Footer() {
    <footer>
 <div className="footer-container">
     <div className="left-footer">
-      <div className="coca-production">COCA PRODUCTION</div>
-      <div className="telefon">+04 0753774477</div>
-      <div className="mail">fakemail@gmail.com</div>
+      <div className="coca-production"><a href="">Something</a></div>
+      <div className="telefon"><a href="tel:+075366412332">0753664167</a></div>
+      <div className="mail"><a href="mailto:someone@yoursite.com">fakemail@gamil.com</a> </div>
     </div>
     <div className="right-footer">
-      <div className="youtube">YOUTUBE</div>
-      <div className="insta">INSTAGRAM</div>
-      <div className="facebook">FACEBOOK</div>
+      <div className="youtube"><a  href="https://www.youtube.com/@cocaproduction" rel='noreferrer' target="_blank">YOUTUBE</a></div>
+      <div className="insta"><a  href="https://instagram.com/cocaproductionfilm?utm_source=qr&igshid=ZDc4ODBmNjlmNQ%3D%3D" rel='noreferrer' target="_blank">INSTAGRAM</a></div>
+      <div className="facebook"><a  href="https://www.facebook.com/profile.php?id=100083285113494" rel='noreferrer' target="_blank">FACEBOOK</a></div>
     </div>
     </div>
    </footer>
