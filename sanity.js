c13e4ba13af8841b40f5a9cac298290882c2b21a
paluras <paluraandrei1@gmail.com>
2025-03-19 11:24:15 +0200

    change production

--- sanity.js
+++ sanity.js
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-    projectId:'hemjxgd3',
+    projectId:'j0sbdb3j',
     dataset:'production',
     useCdn:true,
     apiVersion:'2024-03-25'
